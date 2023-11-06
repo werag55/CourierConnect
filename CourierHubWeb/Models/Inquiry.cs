7a3fd6f728b7363076f6ec8c453b1faf0ae14eae
Weronika Gozdera <weronika.gozdera@gmail.com>
2023-11-06 12:49:10 +0100

    Reaname

---
+++ CourierHubWeb/Models/Inquiry.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourierHubWeb.Models
+{
+    public class Inquiry
+    {
+        [Key] public int Id { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
