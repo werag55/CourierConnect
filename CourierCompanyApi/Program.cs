b263e5e2f9b7aa53b683d05b9779d2fb0571d380
Weronika Gozdera <weronika.gozdera@gmail.com>
2023-11-24 14:41:28 +0100

    Bugs repaired

--- CourierCompanyApi/Program.cs
+++ CourierCompanyApi/Program.cs
@@ -48,16 +48,6 @@ builder.Services.AddSwaggerGen( c =>
 });
 
 builder.Services.AddScoped<ApiKeyAuthFilter>();
-=======
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
