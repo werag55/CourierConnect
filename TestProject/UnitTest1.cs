4ce2a9427e40213d7aa1388ec58f0b5784571677
merbich <grishko.alina1@mail.ru>
2024-01-14 18:21:12 +0100

    unit test project

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace TestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //var controller = new CourierConnectWeb.Controllers.DeliveryController();
+
+        }
+    }
+}
