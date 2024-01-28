06c045c90a505659e96c427a65be60663aa37dbd
merbich <grishko.alina1@mail.ru>
2024-01-28 02:48:01 +0100

    integration test for endpoints

--- CourierConnectWeb/Program.cs
+++ CourierConnectWeb/Program.cs
@@ -77,3 +77,4 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+public partial class Program { }
---
+++ IntegrationTests/UnitTest1.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace IntegrationTests
+{
+    public class BasicTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+        public BasicTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/")]
+        [InlineData("Home/Index")]
+        [InlineData("Home/Privacy")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            //Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal("text/html; charset=utf-8", response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
