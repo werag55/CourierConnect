483a5dc98e2a15e1bc264b4ed2711c32b7fd8844
merbich <grishko.alina1@mail.ru>
2024-01-30 20:30:07 +0100

    Login Selenium Test

--- SeleniumTesting/UnitTest1.cs
+++ SeleniumTesting/UnitTest1.cs
@@ -1,5 +1,6 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 
 namespace SeleniumTesting
 {
@@ -9,13 +10,15 @@ namespace SeleniumTesting
         [SetUp]
         public void Setup()
         {
-            Driver = new ChromeDriver();
+            Driver = new FirefoxDriver();
         }
 
         [Test]
         public void Test1()
         {
-            Driver.Navigate().GoToUrl("https://github.com/");
+            Driver.Navigate().GoToUrl("https://connectcourier.azurewebsites.net/");
+            Driver.FindElement(By.Id("login")).Click();
+
             Assert.Pass();
         }
     }
